d2b724275ac9715c4d38a015fd264f7a085cce31
Rumoers <1347903339@qq.com>
2020-02-21 23:45:56 +0800

    创建Eureka Server

---
+++ coupon-eureka/src/main/java/com/xzh/coupon/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.xzh.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author XZH
+ */
+//表示为EurekaServer
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
